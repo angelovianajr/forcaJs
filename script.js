c5f392e5b8a87e1dddbeac54ef39018a0a76ba31
Pablo Brodt <pablobrodt@gmail.com>
2015-10-23 16:48:51 -0200

    asd

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+var dificuldade = localStorage.getItem('dificuldade');
+var limiteErros;
+
 $('#btnSubmit').click(function() {
   inserirUsuario($('#userName').val(), $('input[name="radioOptions"]:checked').val(), 0);
   console.log($('input[name=RadioOptions]:checked').val());
   pegarPalavra();
 });
 
-function pegarPalavra(){
-  $.get('http://localhost:3000/palavras').done(function(data){
-    palavra = data[parseInt(Math.random()  *10)].texto.split('');
-  })
-}
 
 function inserirUsuario(nome, radio, valor){
+
+  localStorage.setItem('dificuldade', radio);
+
+  limiteErros = ( dificuldade === 'nunez' ) ? 2 : 5;
+
   $.post('http://localhost:3000/usuarios',
     {
       "nome": nome,
@@ -18,4 +21,11 @@ function inserirUsuario(nome, radio, valor){
       "pontos":valor
     }
   );
+}
+
+function pegarPalavra(){
+  $.get('http://localhost:3000/palavras').done(function(data){
+    palavra = data.filter(function(elem){});
+    //palavra = data[parseInt(Math.random()  *10)].texto;
+  })
 }
