3471f8dab78d412a41fa8f3146c516964346c2dc
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-10-24 18:27:42 -0200

    function pegar palavra adicionada

--- OO/Palavra.js
+++ OO/Palavra.js
@@ -1,5 +1,5 @@
-function Palavra( texto ){
-	this.texto = texto;
+function Palavra(){
+	this.texto = pegarPalavra();
 	this.vetor = this.texto.split('');
 	this.oculta = [];
 }
@@ -11,8 +11,26 @@ Palavra.prototype.ocultar = function(){
 			self.oculta.push('*');
 		}else{
 			self.oculta.push(' - ');
-		}	
+		}
 	});
 
 	$('#palavraOculta').text(this.oculta.join(''));
 }
+
+function pegarPalavra(){
+	var palavra;
+  $.get('http://localhost:3000/palavras').done(function(data){
+    var palavrasDisponiveis;
+    if( dificuldade === 'nunez' ){
+      palavrasDisponiveis = data.filter(function(elem){
+        return elem.texto.length >= 12;
+      });
+    }else{
+      palavrasDisponiveis = data;
+    }
+    var index = parseInt(Math.random() * palavrasDisponiveis.length);
+    palavra = palavrasDisponiveis[index].texto;
+		return palavra;
+  });
+
+};
