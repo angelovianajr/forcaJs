9c682bd50f2dcf1a154086aed3766cb61d967620
Pablo Brodt <pablobrodt@gmail.com>
2015-10-25 21:57:17 -0200

    refactor(OO): criar classe Palavra
    adicionados metodos de conversão do texto da palavra para array
    adicionados metodos para popular array da propriedade oculta

--- OO/Palavra.js
+++ OO/Palavra.js
@@ -1,5 +1,24 @@
-function Palavra(){
-	this.texto;
-	this.vetor;
-	this.oculta = [];
+function Palavra( texto ){
+	this.texto = texto;
+	this.vetor = this.textoToArray();
+	this.oculta = this.ocultarTexto();
+};
+
+Palavra.prototype.textoToArray = function(){
+	return this.texto.split('');
+};
+
+Palavra.prototype.ocultarTexto = function(){
+
+	var palavraOculta = [];
+
+	this.vetor.forEach(function(caractere) {
+		if(caractere !== ' '){
+			palavraOculta.push('*');
+		}else{
+			palavraOculta.push(' ');
+		}
+	});
+
+	return palavraOculta;
 };
