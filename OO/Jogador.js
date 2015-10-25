0c958caced4faf52dd3785d681634550351c7097
Pablo Brodt <pablobrodt@gmail.com>
2015-10-25 21:27:19 -0200

    refactor(OO): adicionado metodo inserirUsuario na classe Jogador
    Este método já conta quantos usuários com nome válido existe no json,
    gerando assim um id novo para o usuario a ser inserido

--- OO/Jogador.js
+++ OO/Jogador.js
@@ -1,5 +1,36 @@
-function Jogador(){
-	this.nome;
-	this.pontos;
+function Jogador( nome ){
+	this.nome = nome;
+	this.pontos = 0;
 	this.id;
 };
+
+
+Jogador.prototype.inserirJogador = function(){
+
+	var self = this;
+
+	//Fazer requisição para ver quantos usuarios existem no JSON
+	$.get('http://localhost:3000/usuarios').done(function(data){
+
+		//Faz um reduce para contar a quantidade de usuarios com nome válido
+		ultimoId = data.reduce(function(totalUsuarios, usuario){
+							return usuario.nome !== undefined ? totalUsuarios+1 : totalUsuarios;
+						}, 0);
+
+		self.id = ultimoId+1;
+
+		//Guardar as informações do usuario no localStorage para ser acessado de outras páginas
+		//Aqui também está sendo setado o atributo "palavras" como um array vazio, este array
+		//irá conter as palavras que o uauario acertou
+		localStorage.setItem(self.nome, JSON.stringify({ "id":self.id, "nome":self.nome, "pontos":self.pontos, "palavras":[] }));
+
+		$.post('http://localhost:3000/usuarios',
+    {	
+    	"id": self.id,
+      "nome": self.nome,
+      "pontos":self.pontos
+    });
+	});
+
+};
+
