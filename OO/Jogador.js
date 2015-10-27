c7ec767bb2f4c2d2a45f8c7cd689ce006e7aa4be
Pablo Brodt <pablobrodt@gmail.com>
2015-10-27 09:21:51 -0200

    refactor(forcaJs): adicionada classe Jogador

---
+++ OO/Jogador.js
@@ -0,0 +1,48 @@
+function Jogador( nome ){
+	this.nome = nome;
+	this.pontos = 0;
+	this.id;
+};
+
+
+Jogador.prototype.inserirJogador = function(){
+
+	var self = this;
+
+	//Fazer requisição para ver quantos usuarios existem no JSON
+	$.get('http://localhost:3000/usuarios').done(function(data){
+
+		//Faz um reduce para contar a quantidade de usuarios com nome válido
+		ultimoId = data.reduce(function(totalUsuarios, usuario){
+							return usuario.nome !== undefined ? totalUsuarios+1 : totalUsuarios;
+						}, 0);
+
+		self.id = ultimoId+1;
+
+		//Guardar as informações do usuario no localStorage para ser acessado de outras páginas
+		//Aqui também está sendo setado o atributo "palavras" como um array vazio, este array
+		//irá conter as palavras que o uauario acertou
+		localStorage.setItem(self.nome, JSON.stringify({ "id":self.id, "nome":self.nome, "pontos":self.pontos, "palavras":[] }));
+
+		$.post('http://localhost:3000/usuarios',
+    {	
+    	"id": self.id,
+      "nome": self.nome,
+      "pontos":self.pontos
+    });
+	});
+
+};
+
+
+Jogador.prototype.atualizarUsuario = function(){
+
+	var self = this;
+	
+  $.ajax({
+    url: 'http://localhost:3000/usuarios/'+self.id,
+    type:'PATCH',
+    data: { "nome":self.nome, "pontos":self.pontos }
+  });
+};
+
